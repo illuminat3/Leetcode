fc56b237b80b7b09d9fbcac3a61e3ac225883199
Matthew <matthewrawson2@gmail.com>
2024-02-17 22:43:31 +0000

    Updated to #2 to make it work

--- Packages/AddTwoNumbers.py
+++ Packages/AddTwoNumbers.py
@@ -5,7 +5,7 @@ class Solution:
         carry = 0
         output = []
         blank = ListNode()
-        listNode = ListNode()
+        listNode = ListNode()          
 
         while l1.next != None or l2.next != None or carry != 0:
             num1 = 0 if l1 == None else l1.val
@@ -21,6 +21,16 @@ class Solution:
             l1 = l1.next if l1.next != None else blank
             l2 = l2.next if l2.next != None else blank
         
+        if l1.next == None and l2.next == None and (l1.val != 0 or l2.val != 0):
+            total = l1.val + l2.val
+            if total >= 10:
+                carry = 1
+                total = total - 10
+                output.append(total)
+                output.append(carry)
+            else:
+                output.append(total)
+
         output.reverse()
 
         for i in range(0, len(output)):
