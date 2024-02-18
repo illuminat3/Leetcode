5985ce8530acbccdaf342e8d42196301e7db105e
Matthew <matthewrawson2@gmail.com>
2024-02-18 21:26:58 +0000

    Added Solution

--- Problems/4-Median-of-Two-Sorted-Arrays/MedianOfTwoSortedArrays.py
+++ Problems/4-Median-of-Two-Sorted-Arrays/MedianOfTwoSortedArrays.py
@@ -1,3 +1,8 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        pass
+        nums = sorted(nums1 + nums2)
+        length = len(nums)
+        if length % 2 == 0:
+            return (nums[length // 2] + nums[length // 2 - 1]) / 2
+        else:
+            return nums[length // 2]
