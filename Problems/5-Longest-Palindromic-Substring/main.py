b0bfa52442729533d005866f06bff8048961eead
Matthew <matthewrawson2@gmail.com>
2024-02-18 22:42:17 +0000

    Add test cases for longestPalindrome function

--- Problems/5-Longest-Palindromic-Substring/main.py
+++ Problems/5-Longest-Palindromic-Substring/main.py
@@ -3,3 +3,6 @@ from LongestPalindromicSubstring import Solution
 if __name__ == '__main__':
     solution = Solution()
     print(solution.longestPalindrome("cabad"))
+    print(solution.longestPalindrome("babab"))
+    print(solution.longestPalindrome("caba"))
+    print(solution.longestPalindrome("cbb"))
