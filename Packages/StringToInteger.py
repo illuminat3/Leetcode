6ab81f67f362a2791514dbeeea5b5e42d07dd35d
Matthew Rawson <mrawson@redev.co.uk>
2024-02-21 09:47:10 +0000

    Added #8 Solution to Packages

--- Packages/StringToInteger.py
+++ Packages/StringToInteger.py
@@ -1,3 +1,23 @@
 class Solution:
     def myAtoi(self, s):
-        pass
+        length = len(s)
+        counting = False
+        output = ""
+
+        for i in range(0, length):
+            if not s[i].isdigit():
+                if s[i] == " " and counting == False:
+                    continue
+                if (s[i] == "-" or s[i] == "+") and counting == False:
+                    counting = True
+                    output += s[i]
+                    continue
+                break
+                
+            output += s[i]
+            counting = True
+        
+        if output == "" or output == "+" or output == "-":
+            return 0
+        
+        return max(min(int(output), 2147483647), -2147483648)
