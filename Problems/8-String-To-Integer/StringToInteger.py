7d1b53861d9efaa3fae8e48b6beebc7e0add91b6
Matthew Rawson <mrawson@redev.co.uk>
2024-02-21 09:13:57 +0000

    First Solution

--- Problems/8-String-To-Integer/StringToInteger.py
+++ Problems/8-String-To-Integer/StringToInteger.py
@@ -1,3 +1,24 @@
 class Solution:
     def myAtoi(self, s):
-        pass
+        length = len(s)
+        counting = False
+        output = ""
+        if s.isdigit():
+            return int(s)
+        
+        for i in range(0, length):
+            if not s[i].isdigit():
+                if counting:
+                    break
+                if s[i] == "-" or s[i] == "+":
+                    output += s[i]
+                    counting = True
+                    continue
+            
+            output += s[i]
+            counting = True
+
+        if output == "":
+            return 0
+
+        return max(min(int(output), 2147483647), -2147483647)
