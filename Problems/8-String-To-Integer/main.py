f62db71107983c0a5270e19d423874096f643c0c
Matthew Rawson <mrawson@redev.co.uk>
2024-02-21 09:45:18 +0000

    Added Test Cases

--- Problems/8-String-To-Integer/main.py
+++ Problems/8-String-To-Integer/main.py
@@ -1,2 +1,11 @@
+from StringToInteger import Solution
+
 if __name__ == "__main__": 
-    pass
+    solution = Solution()
+    print(solution.myAtoi("42"))
+    print(solution.myAtoi("   -42"))
+    print(solution.myAtoi("4193 with words"))
+    print(solution.myAtoi("words and 987"))
+    print(solution.myAtoi(".1"))
+    print(solution.myAtoi("00000-42a1234"))
+    print(solution.myAtoi("+0 123"))
